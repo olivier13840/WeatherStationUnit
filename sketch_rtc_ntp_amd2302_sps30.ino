524505d13be85838e8ed1f0408065306c4705082
olivier13840 <olivier.boiron@gmail.com>
2025-10-22 13:01:00 +0200

    Add files via upload

--- sketch_rtc_ntp_amd2302_sps30.ino
+++ sketch_rtc_ntp_amd2302_sps30.ino
@@ -16,8 +16,7 @@
 #define DHTPIN 19        // Broche du signal du capteur AM2302 (DHT22)
 #define DHTTYPE DHT22   // Type du capteur
 
-const unsigned long dureeAcquisition = 2UL * 60UL * 1000UL; // 2 min en ms
-const unsigned long intervalLecture = 1UL * 1000UL; // lecture toutes les 1 s 
+const unsigned long dureeAcquisition = 5UL * 60UL * 1000UL; // 2 min en ms
 int lastMinute = -1;
 
 // === CONFIG SPS30 ===
@@ -28,8 +27,8 @@ SensirionI2cSps30 sensor;
 DHT dht(DHTPIN, DHTTYPE);
 
 // === CONFIG WIFI ===
-const char* ssid = "HONORX7b";
-const char* password = "12345678";
+const char* ssid = "SgOb24";
+const char* password = "azqswx63";
 
 double meanT = 0.0;
 double stdT= 0.0;
@@ -38,6 +37,16 @@ double meanRH= 0.0;
 double stdRH= 0.0;
 double BufferRH[400]; 
 int indexcount;
+uint16_t mc1p0 = 0;
+uint16_t mc2p5 = 0;
+uint16_t mc4p0 = 0;
+uint16_t mc10p0 = 0;
+uint16_t nc0p5 = 0;
+uint16_t nc1p0 = 0;
+uint16_t nc2p5 = 0;
+uint16_t nc4p0 = 0;
+uint16_t nc10p0 = 0;
+uint16_t typicalParticleSize = 0;
 
 DateTime t;
 int minute;
@@ -48,7 +57,7 @@ static int16_t error;
 
 // === CONFIG NTP ===
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "ntp.ec-m.fr", 3600, 60000); // UTC+1
+NTPClient timeClient(ntpUDP, "ntp.unice.fr", 3600, 60000); // UTC+1
 
 // === RTC DS3231 ===
 RTC_DS3231 rtc;
@@ -68,12 +77,11 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("\nConnecté au WiFi");
-
   // Démarrage client NTP
   timeClient.begin();
-
   // Attente de l'heure NTP
   while(!timeClient.update()) {
+    Serial.println("\nAttente synchro NTP..");
     timeClient.forceUpdate();
   }
 
@@ -82,6 +90,8 @@ void setup() {
   struct tm *ptm = gmtime((time_t *)&epochTime);
 
   // Initialisation RTC avec l'heure NTP
+   //Init protocole I2C
+  Wire.begin(21, 22,100000);  // RTC sur SDA=21, SCL=22
   if (!rtc.begin()) {
     Serial.println("Erreur : RTC non détecté !");
     while (1);
@@ -95,68 +105,113 @@ void setup() {
   dht.begin();
 
   // Init I2C + capteur SPS30
-  Wire.begin();
-  sensor.begin(Wire, SPS30_I2C_ADDR_69);
-  int8_t serialNumber[32] = {0};
-  int8_t productType[8] = {0};
-  sensor.readSerialNumber(serialNumber, 32);
-  Serial.print("serialNumber: ");
-  Serial.print((const char*)serialNumber);
-  Serial.println();
-  sensor.readProductType(productType, 8);
-  Serial.print("productType: ");
-  Serial.print((const char*)productType);
-  Serial.println();
+  Wire1.begin(17, 16,100000);
+  sensor.begin(Wire1, SPS30_I2C_ADDR_69);
+  sensor.stopMeasurement();
+  while (Wire.available()) Wire.read();
+  delay(5000);
 }
 // === Boucle principale ===
 void loop() {
-  t = rtc.now();
+  t = rtc.now();  //recupere l'heure
   minute = t.minute();
   heure = t.hour();
 
   Serial.printf("Heure actuelle : %02d:%02d:%02d\n", heure, minute, t.second());
+  delay(2000);
 
   // Vérifie si on est à xx:00 ou xx:30
   if ((minute == 0 || minute == 30) && lastMinute != minute) {
     Serial.println("⏰ Début d’acquisition !");
     Acquisition();
-
     lastMinute = minute;
   }
- 
 }
  
-void Acquisition() {
+void Acquisition() {     //fonction qui recupere les donnees sur les capteurs
   Serial.println("=== Début acquisition ===");
+  uint16_t mc1p0R;
+  uint16_t mc2p5R;
+  uint16_t mc4p0R;
+  uint16_t mc10p0R;
+  uint16_t nc0p5R;
+  uint16_t nc1p0R;
+  uint16_t nc2p5R;
+  uint16_t nc4p0R;
+  uint16_t nc10p0R;
+  uint16_t typicalParticleSizeR;
+  //Initialisation a zero des variables avant acquisition
   meanT=0.;
   meanRH=0.;
   stdT=0.;
   stdRH=0.;
-  indexcount =0;
-  for (int i=0;i<400;i++) {
+  mc1p0 = 0;
+  mc2p5 = 0;
+  mc4p0 = 0;
+  mc10p0 = 0;
+  nc0p5 = 0;
+  nc1p0 = 0;
+  nc2p5 = 0;
+  nc4p0 = 0;
+  nc10p0 = 0;
+  typicalParticleSize = 0;
+
+  uint16_t dataReadyFlag = 0;
+  for (int i=0;i<400;i++) {  //met a zero le buffer pour calculer l'ecart type de la temp et RH
     BufferT[i]=0.0;
     BufferRH[i]=0.0;
   }
-  unsigned long maintenant = millis();
-  unsigned long debutAcquisition = maintenant;
+  indexcount =0;  //nombre d'acquisition
+  
+  unsigned long maintenant = millis(); //on recupere le temps de l'ESP32 !!!
+  unsigned long debutAcquisition = maintenant; //on sauve le temps au demarrage de l'acquisition
+  sensor.startMeasurement(SPS30_OUTPUT_FORMAT_OUTPUT_FORMAT_UINT16); //SPS30 en mode MESURE
+  delay(1000);
 
   // Si on est dans la fenêtre d'acquisition
-  while (maintenant - debutAcquisition < dureeAcquisition) {
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
-
-    if (!isnan(h) && !isnan(t)) {
+  while (maintenant - debutAcquisition < dureeAcquisition) {   //on reste ds la boucle while 5minutes!
+    //lecture AMD2302
+    float h = dht.readHumidity();   //on lit l'humidite
+    float t = dht.readTemperature(); //on lit la temperature
+    if (!isnan(h) && !isnan(t)) {   //on sauve les donnees pour le calcul de la moyenne et de l'ecart type
       Serial.printf("Mesure: T=%.1f°C, RH=%.1f%%\n", t, h);
-      meanT+=t;
+      meanT+=t;  //meanT=meanT+t
       meanRH+=h;
       BufferT[indexcount]=t;
       BufferRH[indexcount]=h;
-      indexcount++;
-      delay(1000);
     }
+
+    //lecture SPS30
+    error = sensor.readDataReadyFlag(dataReadyFlag);
+    if (error != NO_ERROR) {
+      Serial.print("Error trying to execute readDataReadyFlag(): ");
+      errorToString(error, errorMessage, sizeof errorMessage);
+      Serial.println(errorMessage);
+      return;
+    }
+    error = sensor.readMeasurementValuesUint16(mc1p0R, mc2p5R, mc4p0R, mc10p0R,
+                                               nc0p5R, nc1p0R, nc2p5R, nc4p0R,
+                                               nc10p0R, typicalParticleSizeR);
+    if (error != NO_ERROR) {
+      Serial.print("Error trying to execute readMeasurementValuesUint16(): ");
+      errorToString(error, errorMessage, sizeof errorMessage);
+      Serial.println(errorMessage);
+      return;
+    }
+    mc1p0+=mc1p0R; mc2p5+=mc2p5R; mc4p0+=mc4p0R; mc10p0+=mc10p0R;
+    nc0p5+=nc0p5R; nc1p0+=nc1p0R; nc2p5+=nc2p5R; nc4p0+=nc4p0R;
+    nc10p0+=nc10p0R; typicalParticleSize+=typicalParticleSizeR;
+    Serial.print("mc1p0: ");
+    Serial.print(mc1p0);
+    indexcount++;
+
+    //on attend en gros une seconde pour prechaine acquisition
+    delay(1000); 
     maintenant = millis();
   } 
-  // Fin d’acquisition : envoi des données
+  // Fin d’acquisition
+  // Stop SPS30 
+
   Serial.println("=== Fin acquisition, calcul et envoi des données ===");
   meanT=meanT/(double)indexcount;
   meanRH=meanRH/(double)indexcount;
@@ -164,8 +219,15 @@ void Acquisition() {
     stdT+=(meanT-BufferT[i])*(meanT-BufferT[i]);
     stdRH+=(meanRH-BufferRH[i])*(meanRH-BufferRH[i]);
   }
+  //calcul ecart type T et RH
   stdT=sqrt(stdT/double(indexcount));
   stdRH=sqrt(stdRH/double(indexcount));
+
+  // Calcul moyenne PM
+  mc1p0=mc1p0/indexcount; mc2p5=mc2p5/indexcount; mc4p0=mc4p0/indexcount; mc10p0=mc10p0/indexcount;
+  nc0p5=nc0p5/indexcount; nc1p0=nc1p0/indexcount; nc2p5=nc2p5/indexcount; nc4p0=nc4p0/indexcount;
+  nc10p0=nc10p0/indexcount; typicalParticleSize=typicalParticleSize/indexcount;
+ // Envoi des données
   envoyerDonnees();
 } 
 
@@ -180,6 +242,37 @@ void envoyerDonnees() {
   Serial.print(F(" °C |  Température STD: "));
   Serial.print(stdT);
   Serial.println(F(" °C"));
+
+  Serial.print("mc1p0: ");
+  Serial.print(mc1p0);
+  Serial.print("\t");
+  Serial.print("mc2p5: ");
+  Serial.print(mc2p5);
+  Serial.print("\t");
+  Serial.print("mc4p0: ");
+  Serial.print(mc4p0);
+  Serial.print("\t");
+  Serial.print("mc10p0: ");
+  Serial.print(mc10p0);
+  Serial.print("\t");
+  Serial.print("nc0p5: ");
+  Serial.print(nc0p5);
+  Serial.print("\t");
+  Serial.print("nc1p0: ");
+  Serial.print(nc1p0);
+  Serial.print("\t");
+  Serial.print("nc2p5: ");
+  Serial.print(nc2p5);
+  Serial.print("\t");
+  Serial.print("nc4p0: ");
+  Serial.print(nc4p0);
+  Serial.print("\t");
+  Serial.print("nc10p0: ");
+  Serial.print(nc10p0);
+  Serial.print("\t");
+  Serial.print("typicalParticleSize: ");
+  Serial.print(typicalParticleSize);
+  Serial.println();
 
 }
  
